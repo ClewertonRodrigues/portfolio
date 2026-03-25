f58ea6c7cb03459005cae7331c7b279c16307625
clewertondev <clewertonrodrigues93819@gmail.com>
2026-03-25 12:35:46 -0300

    style: melhora visibilidade dos icones de tecnologias no mobile

--- app/components/technology/index.tsx
+++ app/components/technology/index.tsx
@@ -11,7 +11,7 @@ export function Technology({ icon, name }: TecnologiaProps) {
       className="bg-[#0F172A] flex flex-col gap-2 items-center p-4 border border-[#1F2937] rounded-lg group transition-all duration-500 hover:-translate-y-2 hover:border-[#6366F1] hover:shadow-xl hover:shadow-[#6366F1]/20 "
       data-aos="zoom-in"
     >
-      <div className="opacity-80 group-hover:opacity-100 group-hover:scale-110">
+      <div className="lg:opacity-80 group-hover:opacity-100 group-hover:scale-110">
         {icon}
       </div>
       <p
